60b2a9771b59c3c7d2d961b11d5eda916812e495
Ashish Verma <ashishkv.2016@gmail.com>
2022-04-19 19:37:02 +0530

    register and login backend nearly done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
+const AuthRoute = require("./routes/AuthRoute");
+
+mongoose.connect(MONGO_URI).then(
+  () => {
+    console.log("connected to database..");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+app.use(express.json());
+app.get("/", (req, res) => res.send("server is up and running..."));
+app.use("/api/auth", AuthRoute);
+app.listen(PORT, () => console.log("server started"));

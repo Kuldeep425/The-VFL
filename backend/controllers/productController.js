91bd551daeda2823bca5309a6e8de5d71fb0730c
Ashish Verma <ashishkv.2016@gmail.com>
2022-04-20 23:14:39 +0530

    cart backend done

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+const Product = require("../models/productModel");
+
+const addProduct = (req, res) => {};
+
+const removeProduct = (req, res) => {};
+
+const updateProduct = (req, res) => {};
+
+const viewMyProducts = (req, res) => {};
+
+const productsByCategory = (req, res) => {};
+
+const productsByTag = (req, res) => {};
+
+module.exports = {
+  addProduct,
+  removeProduct,
+  updateProduct,
+  viewMyProducts,
+  productsByCategory,
+  productsByTag,
+};

5663f8b437051fbd789c63800837d1d4b5e7ad44
Ashish Verma <ashishkv.2016@gmail.com>
2022-04-21 11:36:21 +0530

    products controller updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,42 +1,57 @@
 const Product = require("../models/productModel");
-const User=require('../models/UserModel')
+const User = require("../models/UserModel");
 
-const addProduct = async(req, res) => {
-  const sid=req.params.sid;
-  const seller = await User.findById(sid)
-  if(!seller) return res.status(404).send("some error occured")
-  const newProd = await Product.create({...req.body,})
-  if(!newProd)  return res.status(504).send("some error occured")
-  let products=seller._doc.myProducts;
-  products=[...products,newProd._doc.id]
-  User.findByIdAndUpdate(sid,{myProducts:products},function(e,doc){
-    if(e) return res.status(400).send("some error occured")
-    res.send("added successfully")
-  })
+const addProduct = async (req, res) => {
+  const sid = req.params.sid;
+  const seller = await User.findById(sid);
+  if (!seller) return res.status(404).send("some error occured");
+  const newProd = await Product.create({ ...req.body });
+  if (!newProd) return res.status(504).send("some error occured");
+  let products = seller._doc.myProducts;
+  products = [...products, newProd._doc.id];
+  User.findByIdAndUpdate(sid, { myProducts: products }, function (e, doc) {
+    if (e) return res.status(400).send("some error occured");
+    res.send("added successfully");
+  });
 };
 
-const removeProduct = (req, res) => {
-  const sid=req.params.sid, pid=req.params.pid;
-  const seller = await User.findById(sid)
-  if(!seller) return res.status(404).send("some error occured")
-  const products=seller._doc.myProducts;
-  const idx=products.indexOf(pid)
-  if(idx> -1){
-    products.splice(idx,1);
-    User.findByIdAndUpdate(sid,{myProducts:products},function(e,doc){
-      if(e) return res.status(400).send("error occured")
-      res.send("removed successfully")
-    })
-    Product.findByIdAndDelete(pid,function(e,doc){
-      if(e) return res.status(404).send("product not found")
-      res.send("removed successfully")
-    })
+const removeProduct = async (req, res) => {
+  const sid = req.params.sid,
+    pid = req.params.pid;
+  const seller = await User.findById(sid);
+  if (!seller) return res.status(404).send("some error occured");
+  const products = seller._doc.myProducts;
+  const idx = products.indexOf(pid);
+  if (idx > -1) {
+    products.splice(idx, 1);
+    User.findByIdAndUpdate(sid, { myProducts: products }, function (e, doc) {
+      if (e) return res.status(400).send("error occured");
+      res.send("removed successfully");
+    });
+    Product.findByIdAndDelete(pid, function (e, doc) {
+      if (e) return res.status(404).send("product not found");
+      res.send("removed successfully");
+    });
   }
 };
 
-const updateProduct = (req, res) => {};
+const updateProduct = async (req, res) => {
+  const sid = req.params.sid,
+    pid = req.params.pid;
+  const product = await Product.findById(pid);
+  //Check whether the product owner is updating the product
+  if (!product || product._doc.sellerId != sid)
+    return res.status(404).send("some error occured");
+  Product.findByIdAndUpdate(pid, { ...req.body }, function (e, doc) {
+    if (e) return res.status(404).send("some error occured");
+    res.send("successfully updated");
+  });
+};
 
-const viewMyProducts = (req, res) => {};
+const viewMyProducts = async (req, res) => {
+  const sid = req.params.sid;
+  // const Products = await Product.find()
+};
 
 const productsByCategory = (req, res) => {};
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -11,8 +11,8 @@ const Router = require("express").Router();
 
 Router.post("/add/:sid", (req, res) => addProduct(req, res)); //seller id: sid
 Router.get("/remove/:sid/:pid", (req, res) => removeProduct(req, res));
-Router.post("/updateProduct/:pid", (req, res) => updateProduct(req, res));
-Router.get("/view/:uid", (req, res) => viewMyProducts(req, res));
+Router.post("/updateProduct/:sid/:pid", (req, res) => updateProduct(req, res));
+Router.get("/view/:sid", (req, res) => viewMyProducts(req, res));
 Router.get("/viewByCategory/:cat", (req, res) => productsByCategory(req, res));
 Router.get("/viewByTag/:tag", (req, res) => productsByTag(req, res));
 
